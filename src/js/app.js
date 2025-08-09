106199c81f6d6431eb019e8791336a1611ae8da8
Alexmig24 <alexandermqc2004@hotmail.com>
2025-08-09 17:05:02 -0500

    update the files the download the PWA

--- src/js/app.js
+++ src/js/app.js
@@ -1,63 +1,43 @@
-let MAIN;
-let MODAL_POST;
-let BTN_SHOW_POST;
-let BTN_CANCEL_POST;
 let deferredPrompt;
-// Funciones
-const showPostModal = () => {
-  MAIN.style.display = "none"; // Oculta el contenido principal
-  MODAL_POST.style.display = "block"; // Muestra el modal
-  setTimeout(() => {
-    MODAL_POST.style.transform = "translateY(0)"; // Animación para mostrar el modal
-  }, 1);
-};
 
-const closePostModal = () => {
-  MAIN.style.display = "block";
-  MODAL_POST.style.transform = "translateY(100vh)"; // Oculta el modal con animación
-};
-
+// Evento para instalación de PWA
 window.addEventListener("beforeinstallprompt", (e) => {
-  console.log("Evento por defecto anulado");
-  e.preventDefault(); // Previene el comportamiento por defecto
-  deferredPrompt = e; // Guarda el evento para usarlo más tarde
+  e.preventDefault();
+  deferredPrompt = e;
 });
 
-// Cuando se carga el DOM
+// Inicialización
 window.addEventListener("load", async () => {
-  MAIN = document.querySelector("#main");
-  MODAL_POST = document.querySelector("#modal-post-section");
-  BTN_SHOW_POST = document.querySelector("#btn-upload-post");
-  BTN_SHOW_POST.addEventListener("click", showPostModal); // Asigna el evento al botón de mostrar post
-  BTN_CANCEL_POST = document.querySelector("#btn-post-cancel");
-  BTN_CANCEL_POST.addEventListener("click", closePostModal); // Asigna el evento al botón de cerrar post
-  
-  await Notification.requestPermission(); //Solicita permisos para notificaciones
-  
+  // Solicitar permiso de notificaciones
+  await Notification.requestPermission();
+
+  // Registrar Service Worker
   if (navigator.serviceWorker) {
     const basePath = location.hostname === "localhost" ? "" : "/PWA_Material-Design";
     try {
       const res = await navigator.serviceWorker.register(`${basePath}/sw.js`);
       if (res) {
-        console.log("Service Worker registered successfully.");
+        console.log("Service Worker registrado correctamente.");
         const ready = await navigator.serviceWorker.ready;
-        ready.showNotification("ESPE Notes", {
-          body: "Notifcaciones Activadas",
-          icon: "./src/assets/icons/icon-128x128.png",
-          vibrate: [100, 50 , 200],
+        ready.showNotification("Quick Jot", {
+          body: "Notificaciones activadas",
+          icon: "/src/assets/icons/icon-128x128.png",
+          vibrate: [100, 50, 200],
         });
       }
     } catch (error) {
-      console.error("Service Worker registration failed:", error);
-    }
-  }
+      console.error("Error al registrar el Service Worker:", error);
+    }
+  }
+
+  // Evento para instalar PWA
   const bannerInstall = document.querySelector("#banner-install");
   bannerInstall.addEventListener("click", async () => {
     if (deferredPrompt) {
-      deferredPrompt.prompt(); // Muestra el prompt de instalación
-      const response = await deferredPrompt.userChoice; // Espera a que el usuario elija
+      deferredPrompt.prompt();
+      const response = await deferredPrompt.userChoice;
       if (response.outcome === "accepted") {
-        console.log("User accepted the installation prompt");
+        console.log("El usuario aceptó la instalación");
       }
     }
   });
